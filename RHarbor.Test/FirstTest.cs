9583a46e4f37719a618e254ec60a2dcbc9bfa33c
Kenji YAMADA <kenzauros@users.noreply.github.com>
2020-05-07 18:34:10 +0900

    Fix test project

--- RHarbor.Test/FirstTest.cs
+++ RHarbor.Test/FirstTest.cs
@@ -26,31 +26,23 @@ namespace kenzauros.RHarbor.Test
         public void A01_FirstTest()
         {
             var connectionInfo = new PasswordConnectionInfo(FIRST_HOP_HOST, FIRST_HOP_PORT, SSH_USERNAME1, SSH_PASSWORD1);
-            using (var client = new SshClient(connectionInfo))
-            {
-                client.Connect();
-                using (var forward = new ForwardedPortLocal(LOCAL_HOST, SECOND_HOP_HOST, SECOND_HOP_PORT))
-                {
-                    client.AddForwardedPort(forward);
-                    forward.Start();
-                    var connectionInfo2 = new PasswordConnectionInfo(forward.BoundHost, (int)forward.BoundPort, SSH_USERNAME1, SSH_PASSWORD1);
-                    using (var client2 = new SshClient(connectionInfo2))
-                    {
-                        client2.Connect();
-                        var command = client2.RunCommand("ls -la");
-                        Assert.That(command.Result, Is.Not.Empty);
-                        command.Dispose();
-                        using (var forward2 = new ForwardedPortLocal(LOCAL_HOST, REMOTE_IP, REMOTE_PORT))
-                        {
-                            client2.AddForwardedPort(forward2);
-                            forward2.Start();
-                            Assert.That(forward2.IsStarted, Is.True);
-                            Assert.That(forward2.BoundHost, Is.EqualTo(LOCAL_HOST));
-                            Assert.That(forward2.BoundPort, Is.GreaterThan(0));
-                        }
-                    }
-                }
-            }
+            using var client = new SshClient(connectionInfo);
+            client.Connect();
+            using var forward = new ForwardedPortLocal(LOCAL_HOST, SECOND_HOP_HOST, SECOND_HOP_PORT);
+            client.AddForwardedPort(forward);
+            forward.Start();
+            var connectionInfo2 = new PasswordConnectionInfo(forward.BoundHost, (int)forward.BoundPort, SSH_USERNAME1, SSH_PASSWORD1);
+            using var client2 = new SshClient(connectionInfo2);
+            client2.Connect();
+            var command = client2.RunCommand("ls -la");
+            Assert.That(command.Result, Is.Not.Empty);
+            command.Dispose();
+            using var forward2 = new ForwardedPortLocal(LOCAL_HOST, REMOTE_IP, REMOTE_PORT);
+            client2.AddForwardedPort(forward2);
+            forward2.Start();
+            Assert.That(forward2.IsStarted, Is.True);
+            Assert.That(forward2.BoundHost, Is.EqualTo(LOCAL_HOST));
+            Assert.That(forward2.BoundPort, Is.GreaterThan(0));
         }
     }
 }
